a7a9bfb67a25eb5c06412df3d9e453fcff5d2b52
IanRaRev <ianrarev@comunidad.unam.mx>
2020-09-21 15:15:43 -0500

    Con programación reactiva se hicieron las bases para un  predictor de palabras con ajax

--- wishList-ng/src/app/form-pokemon/form-pokemon.component.ts
+++ wishList-ng/src/app/form-pokemon/form-pokemon.component.ts
@@ -1,6 +1,11 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { selectPokemon } from '../models/SelectPokemon.model';
 import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
+import { fromEvent } from 'rxjs';
+import { map, filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
+
 
 @Component({
   selector: 'app-form-pokemon',
@@ -13,6 +18,7 @@ export class FormPokemonComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<selectPokemon>;
   fg: FormGroup;
   minLong = 3;
+  searchResult: string[];
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
@@ -30,6 +36,26 @@ export class FormPokemonComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    const elemNombre = <HTMLInputElement>document.getElementById('nombre');
+    fromEvent(elemNombre, 'input') /*  Observable de eventos de entrada RXJS */
+      .pipe(
+        //1. Detectamos tecla presionada
+        map((e: KeyboardEvent) => (e.target as HTMLInputElement).value),
+        //2. Longitud de la cadena 
+        filter(text => text.length > 2),
+        //3. Intervalo de tiempo entre captura de eventos
+        debounceTime(200),
+        //4. Comparación de la cadena de entrada
+        distinctUntilChanged(),
+        //5. API de busqueda
+        switchMap(() => ajax('/assets/datos.json'))
+      ).subscribe(AjaxResponse => {
+        console.log(AjaxResponse.response);
+        console.log(AjaxResponse);
+        this.searchResult = AjaxResponse.response;
+      })
+    
+    //3. 
   }
 
   guardar(nombre:string, url: string): boolean{
